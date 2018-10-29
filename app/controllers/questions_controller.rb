6afe26ddeeeb551b91bc407b745ec0d75b536330
Stephanie Shahini <stelili@hotmail.com>
2018-10-29 15:45:27 -0300

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,12 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work" then @answer = "Great"
+    elsif @question.include? "?" then @answer == "Silly question, get dressed and go to work!"
+    else @answer = " I don't care, get dressed and go to work!"
+    end
+  end
+end
